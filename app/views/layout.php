21c0b74523246a9ef06d47059e4a3b81be50d73b
OxsanaP <oksanka-vredina@yandex.ru>
2017-03-29 18:08:58 +0300

    add bootstrap

--- app/views/layout.php
+++ app/views/layout.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="/css/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <title>Главная</title>
 </head>
 <body>
 <?php include BP . "/app/views/". $contentView; ?>
+<script src="/js/bootstrap.min.js"></script>
 </body>
 </html>
